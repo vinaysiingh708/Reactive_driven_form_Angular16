cd4b1563cdc887e39a21f9c78487e6980c0522ce
vinay singh <vinaysingh708@yahoo.com>
2024-08-27 18:34:25 +0530

    Add project files.

---
+++ src/app/Validators/noSpaceAllowed.validator.ts
@@ -0,0 +1,31 @@
+import { AbstractControl, FormControl } from "@angular/forms";
+
+export class CustomValidators{
+
+    static noSpaceAllowed(control : FormControl){
+
+        if(control.value !=null && control.value.indexOf(' ') != -1){
+            return {noSpaceAllowed: true}
+        }
+        return null;
+    }
+
+    static checkUserName(control: AbstractControl):Promise<any>{
+        return userNameAllowed(control.value);
+    }
+}
+
+function userNameAllowed(username: string){
+    const takenUserNames= ['ravi','vinay','ankita'];
+
+    return new Promise((resolve,reject) =>  {
+        setTimeout(() => {
+                    if(takenUserNames.includes(username)){
+                        resolve({checkUsername: true});
+                    }else{
+                        resolve(null);
+                    }
+        }, 5000);
+
+    })
+}
